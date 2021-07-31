73ac6141e9c79f1c717af8474fdd3c5b0ad7cf1f
matsikora <35769705+matsikora@users.noreply.github.com>
2021-07-31 18:34:13 +0200

    Add files via upload

---
+++ GlycoSASA-0.1.py
@@ -0,0 +1,360 @@
+__author__ = "Matt Sikora"
+__copyright__ = "Copyright 2021, GlycoSHIELD project"
+__credits__ = ["Cyril Hanus"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Matt Sikora"
+__email__ = "masikora@biophys.mpg.de"
+__status__ = "Development"
+
+
+import numpy as np
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.rms import rmsd
+from MDAnalysis.analysis.rms import RMSF
+from MDAnalysis.analysis.base import AnalysisFromFunction
+import MDAnalysis.analysis.distances as distances
+#from __future__ import print_function
+import shlex
+import os,subprocess,glob
+from shutil import copyfile
+import collections
+from argparse import ArgumentParser
+
+# Define modified amino acids (approximation: we turn them back to the original residues)
+AMINO_ACID_VARIANTS = [
+    ("ALA", ["AIB", "ALM", "AYA", "BNN", "CHG", "CSD", "DAL","DHA", "DNP", "FLA", "HAC", "MAA", "PRR", "TIH", "TPQ",]),
+    ("ARG", ["AGM", "DAR", "HAR", "MMO", "ARM", "ARN", "HMR", "ACL"]),
+    ("ASN", ["MEN", "DSG",]),
+    ("ASP", ["DSP", "BHD", "2AS", "ASQ", "ASB", "ASA", "ASK", "ASH","ASL", "DAS"]),
+    ("CYS", ["BCS", "BUC", "C5C", "C6C", "CCS", "CEA", "CME", "CSO", "CSP", "CSS", "CSW", "CSX", "CY1", "CY3", "CYG", "CYM", "CYP", "CYQ", "CYX", "DCY", "EFC", "OCS", "PEC", "PR3", "SCH", "SCS", "SCY", "SHC", "SMC", "SOC"]),
+    ("GLU", ["GLH", "GGL", 'PCA', '5HP', 'DGL', 'CGU', 'GMA']),
+    ("GLN", [("DGN", "X")]),
+    ("GLY", ["GLZ", "SAR", 'NMC', 'GL3', 'GSC', 'MPQ', 'MSA']),
+    ("HIS", ["DHI", "HID", "HIC", "HIE", "HIP", "HSD", "HSE","HSP", "MHS", "NEM", "NEP", "3AH"]),
+    ("ILE", ['DIL', 'IIL']),
+    ("LEU", ["BUG", "NLE", 'NLP', 'NLN', 'DLE', 'CLE', 'MLE']),
+    ("LYS", ['LYM', 'ALY', 'LYZ', 'LYN', 'LLY', 'LLP', 'SHR', 'TRG', 'DLY', 'KCX']),
+    ("MET", ["FME", "CXM", "OMT", "MSE"]),
+    ("PHE", ["DAH", "DPN", "HPQ", "PHI", "PHL"]),
+    ("PRO", ['DPR', 'HYP']),
+    ("SER", ['OAS', 'MIS', 'SAC', 'SVA', 'SET', 'SEP', 'SEL', "DSN"]),
+    ("THR", ["ALO", "BMT", "DTH", "THO", "TPO"]),
+    ("TRP", ["DTR", "HTR", "LTR", "TPL", "TRO"]),
+    ("TYR", ["DTY", "IYR", "PAQ", "PTR", "STY", "TYB", "TYM", "TYO", "TYQ", "TYS", "TYY"]),
+    ("VAL", ["DIV", "DVA", "MVA"])
+]
+
+AMINO_ACID_VARIANTS_SUBSTITUTION = {}
+for resline in AMINO_ACID_VARIANTS:
+   res = resline[0]
+   for modres in resline[1]:
+      AMINO_ACID_VARIANTS_SUBSTITUTION[modres]=res
+
+
+
+def get_SASA(pdbname,xtcname,index,groupname,selectgroups,outfile,outfileres,outfileatom,probe,ndots,endtime):
+    """
+    Use gromacs to calculate SASA.
+    Tested with GMX 2019.x
+    requires sourced GMXRC (gmx command in the PATH).
+    
+    selectgroups separated by ;
+    
+    The output consists of a text file that assigns a shielding value to each residue. These values multiplied by 100 are also 
+    written into the b-factor column of the PDB file. Residues that are not visible to the probe at all are further marked by 0
+    in the occupancy column of the PDB file. The output files are named: maxResidueSASA_probe_RPROBE.txt maxResidueSASA_probe_RPROBE.pdb, 
+    where RPROBE is a probe radius.
+
+
+   # there is also ndots argument, default to 15, regulating the number of dots in Shrake-Rupley algorithm. https://www.sciencedirect.com/science/article/abs/pii/0022283673900119?via%3Dihub
+    """
+    # Build a gromacs command
+    cmd="gmx sasa -f {} -s {} -n {} -o {} -or {}".format(xtcname,pdbname,index,outfile,outfileres)
+    cmd=cmd+" -oa {}".format(outfileatom)
+    cmd=cmd+" -surface \'{}\' -xvg none -probe {} ".format(groupname,probe)
+    cmd=cmd+" -output \'{}\' -ndots {}".format(selectgroups,ndots)
+    cmd=cmd+" -e {}".format(endtime)
+    print(cmd)
+    
+    # Execute
+    command = shlex.split(cmd)
+    proc = subprocess.Popen(command, stdout = subprocess.PIPE,stderr=subprocess.PIPE)
+    out=[i for i in proc.stdout]
+    err=[i for i in proc.stderr]
+    proc.communicate()
+
+    if proc.returncode!=0:
+        for line in err:
+            print(line)
+            raise SystemExit("Error in Gromacs, stopping...")
+            
+    # Read in the gmx output files
+    sasas=np.loadtxt(outfileres)
+    sasaa=np.loadtxt(outfileatom)
+    sasatime=np.loadtxt(outfile)
+    return sasatime,sasas,sasaa,proc.returncode   
+
+def GMXTEST():
+    cmd='gmx --version --quiet | grep "GROMACS version"'
+    command = shlex.split(cmd)
+    try:
+       proc = subprocess.Popen(command, stdout = subprocess.PIPE,stderr=subprocess.PIPE)
+    except:
+       raise BaseException("Gromacs not found or not working, stopping...")
+    out=[i for i in proc.stdout]
+    err=[i for i in proc.stderr]
+    proc.communicate()
+
+    if proc.returncode!=0:
+        for line in err:
+            print(line)
+            raise SystemExit("Gromacs not found, stopping...")   
+   
+def main(pdbfiles,xtcfiles,plottrace,probes,ndots,mode,keepoutput,endtime):
+   # Assumption is there is only a protein in the pdb file.
+   # Everything else assumed to be a glycan.
+
+
+   # Iterate over probe sizes
+   for probe in probes:
+      outrelativesasa=[]
+      outrelativesasaaa=[]      
+      
+      
+      # Iterate over trajectories for glycans
+      # Load each glycan+protein trajectory
+      for iglycan in range(len(xtcfiles)):
+          
+          pdb=pdbfiles[iglycan]
+          xtc=xtcfiles[iglycan]
+          u=mda.Universe(pdb,xtc)
+          
+          # Define some temporary file names.
+          tmpindex='index0.ndx'
+          tmpsel='prot; gly'
+          tmpsys='system'
+          baresel='prot'
+          tmpsasa  = 'sasa1.xvg'
+          tmpsasar = 'sasar.xvg'
+          tmpsasaa = 'sasaa.xvg'
+          tmppdb = 'test1.pdb'
+          tmpbarepdb = 'test2.pdb'
+          tmpbarextc = 'test2.xtc'
+          
+          # remove all lines but ATOM from the PDB (otherwise gmx sasa freezes)
+          
+          with open(tmppdb,'w') as f:
+             with open(pdb,'r') as g:
+                for line in g: 
+                   if "ATOM" in line:
+                      
+                      # Replace modified AA with natural counterparts
+                      myres = line[17:20]
+                      if myres in AMINO_ACID_VARIANTS_SUBSTITUTION.keys():
+                        line.replace(myres,AMINO_ACID_VARIANTS_SUBSTITUTION[myres])
+                        
+                      f.write(line)
+                      
+          # Re-open the pdb
+          u=mda.Universe(tmppdb,xtc)
+          
+          sel_P=u.select_atoms('protein')
+          sel_P.write(tmpbarepdb)
+          
+          # If the protein is static, bare SASA does not matter. But if we just calc relative sasa then we average over trajectory
+          with mda.coordinates.XTC.XTCWriter(tmpbarextc,n_atoms=sel_P.atoms.n_atoms) as w:
+               for tp in u.trajectory:
+                  w.write(sel_P.atoms)
+          
+          sel_G=u.select_atoms('not protein')
+          
+          
+          with mda.selections.gromacs.SelectionWriter(tmpindex) as w:
+              w.write(sel_P,name='prot')
+              w.write(sel_G,name='gly')
+              w.write(u.atoms,name='system')
+          
+          sasatimes,sasar,sasaa,_ = get_SASA(tmppdb,xtc,tmpindex,tmpsys,tmpsel,tmpsasa,tmpsasar,tmpsasaa,probe,ndots,endtime)
+          
+          if keepoutput:
+              out1name=pdb.replace(".pdb","")
+              copyfile(tmpsasa,"raw_{}_probe_{}.xvg".format(out1name,probe))
+              copyfile(tmpsasar,"raw_r_{}_probe_{}.xvg".format(out1name,probe))
+              copyfile(tmpsasaa,"raw_a_{}_probe_{}.xvg".format(out1name,probe))
+              
+          # Now protein only, this will not change so we calc only once
+          if iglycan==0:
+              baresasatimes,baresasar,baresasaa,_ = get_SASA(tmpbarepdb,tmpbarextc,tmpindex,baresel,baresel,tmpsasa,tmpsasar,tmpsasaa,probe,ndots,endtime)
+              
+              if keepoutput:
+                 copyfile(tmpsasa,"bare_probe_{}.xvg".format(probe))
+                 copyfile(tmpsasar,"bare_r_probe_{}.xvg".format(probe))
+                 copyfile(tmpsasaa,"bare_a_probe_{}.xvg".format(probe))
+                 
+              
+          # Calculate SASA relative to bare protein
+          
+          # to eliminate warnings:
+          baresasar_idx=np.where(baresasar[:,1]!=0)
+          
+          # write down which atoms do not see any probe. Occupancy=0 means that.
+          occupancy_r=np.zeros(len(baresasar[:,1]))
+          occupancy_r[baresasar_idx]=1.0
+          
+          relativesasa=np.zeros(len(baresasar[:,1]))
+          relativesasa[baresasar_idx]=(baresasar[:,1][baresasar_idx]-sasar[:baresasar.shape[0],1][baresasar_idx])/baresasar[:,1][baresasar_idx]
+
+          # all atom version
+          
+          # to eliminate warnings:
+          baresasaa_idx=np.where(baresasaa[:,1]!=0)          
+          
+          # write down which atoms do not see any probe. Occupancy=0 means that.
+          occupancy_a=np.zeros(len(baresasaa[:,1]))
+          occupancy_a[baresasaa_idx]=1.0
+
+          relativesasaaa=np.zeros(len(baresasaa[:,1]))
+          relativesasaaa[baresasaa_idx]=(baresasaa[:,1][baresasaa_idx]-sasaa[:baresasaa.shape[0],1][baresasaa_idx])/baresasaa[:,1][baresasaa_idx]
+
+          # Aappend relative SASA for each residue
+          outrelativesasa.append(relativesasa)
+          # and for each atom
+          outrelativesasaaa.append(relativesasaaa)
+          
+          
+          
+          # cleanup
+          os.remove(tmpindex)
+          os.remove(tmpsasa)
+          os.remove(tmpsasar)
+          os.remove(tmpsasaa)
+          for afile in glob.glob("#sasa*"):
+             os.remove(afile)
+          
+  
+         
+      outrelativesasa=np.array(outrelativesasa)
+      outrelativesasaaa=np.array(outrelativesasaaa)
+
+
+      # mean or max?
+      meanSASA=np.mean(outrelativesasa,axis=0)
+      meanSASAaa=np.mean(outrelativesasaaa,axis=0)
+      maxSASA=np.max(outrelativesasa,axis=0)
+      maxSASAaa=np.max(outrelativesasaaa,axis=0)
+      
+      
+      # adjust output to the mode # CHANGE - OUTPUT BOTH
+      if mode=="avg":
+         writeSASA=meanSASA
+         writeSASAaa=meanSASAaa
+      elif mode == "max":
+         writeSASA=maxSASA
+         writeSASAaa=maxSASAaa
+         
+         
+         
+      # make a per-residue plot of the sasa
+      if plottrace:
+         residues = np.unique(sel_P.resids)
+         
+         plt.clf()
+         i=0
+         for sasa in outrelativesasa:
+             i+=1
+             plt.plot(residues, sasa,lw=0.36,label='glycan {}'.format(i))
+             
+             plt.xlabel('residue No')
+             plt.ylabel('Shielding')
+         plt.plot(residues,maxSASA,c='k',ls='--',label='max')
+         plt.plot(residues,meanSASA,c='gray',ls='-',label='avg')
+
+         plt.legend()
+         plt.ylim(0,1)
+         plt.savefig('{}ResidueSASA_probe_{}.pdf'.format(mode,probe))
+
+
+      # Move back to the reference frame
+      # Re-open the pdb
+      unw=mda.Universe(tmppdb)
+      sel_Pnw=unw.select_atoms('protein')
+
+      if mode == "avg":
+         # write to tempfactors
+         for iresidue in range(len(sel_P.residues)):
+             sel_Pnw.residues[iresidue].atoms.tempfactors=meanSASA[iresidue]*100
+             sel_Pnw.residues[iresidue].atoms.occupancies=occupancy_r[iresidue]
+             
+         sel_Pnw.write('avgResidueSASA_probe_{}.pdb'.format(probe))
+         np.savetxt('avgResidueSASA_probe_{}.txt'.format(probe),meanSASA)
+      
+#      # now write the all-atom values
+#      sel_Pnw.atoms.tempfactors=meanSASAaa*100
+#      sel_Pnw.atoms.occupancies=occupancy_a
+#      sel_Pnw.write('avgAtomSASA_probe_{}.pdb'.format(probe))
+#      np.savetxt('avgAtomSASA_probe_{}.txt'.format(probe),meanSASAaa)
+      if mode == "max":
+         # now maxima:
+         for iresidue in range(len(sel_P.residues)):
+             sel_Pnw.residues[iresidue].atoms.tempfactors=maxSASA[iresidue]*100
+             sel_Pnw.residues[iresidue].atoms.occupancies=occupancy_r[iresidue]
+             
+         sel_Pnw.write('maxResidueSASA_probe_{}.pdb'.format(probe))
+         np.savetxt('maxResidueSASA_probe_{}.txt'.format(probe),maxSASA)
+      
+#      # now write the all-atom values
+#      sel_Pnw.atoms.tempfactors=maxSASAaa*100
+#      sel_Pnw.atoms.occupancies=occupancy_a
+#      sel_Pnw.write('maxAtomSASA_probe_{}.pdb'.format(probe))
+#      np.savetxt('maxAtomSASA_probe_{}.txt'.format(probe),maxSASAaa)
+
+   # cleanup
+   os.remove(tmpbarepdb)
+   os.remove(tmppdb)
+   os.remove(tmpbarextc)
+      
+if __name__=="__main__":
+   parser=ArgumentParser()
+   parser.add_argument('--pdblist', dest='pdblist', help='coma-separated list of pdb references [no spaces! test1.pdb,test2.pdb ]')
+   parser.add_argument('--xtclist', dest='xtclist', help='coma-separated list of xtc trajs [no spaces! test1.xtc,test2.xtc ]')
+   parser.add_argument('--probelist', dest='probelist', help='coma-separated list of probe sizes for SASA calculation. can be a single one')
+   
+   parser.add_argument('--plottrace', dest='plottrace', action='store_true', help='plot per-residue shielding? default is no')
+   parser.add_argument('--no-plottrace', dest='plottrace', action='store_false', help='do not plot per-residue shielding')
+      
+   parser.add_argument('--keepoutput', dest='keepoutput', action='store_true',help="do not delete tmp files, default is no")
+   parser.add_argument('--no-keepoutput', dest='keepoutput', action='store_false',help="delete tmp files")  
+   
+   parser.add_argument('--ndots', dest='ndots', help='name of the plot for per-residue trace of SASA, can be None [TODO- finish]',default=15)
+   parser.add_argument('--mode', dest='mode', help='mode = avg or max, report max or avg Delta_sasa',default="max")
+   parser.add_argument('--endframe', dest='endframe', help='last frame to read from the glycan trajectories. Assumes 1 ps time step',default=0)
+   
+   # GMX required, test if present
+   GMXTEST()
+   
+   args = parser.parse_args()
+   pdbfiles=[i for i in args.pdblist.split(",")]
+   xtcfiles=[i for i in args.xtclist.split(",")]
+   probes=[float(i) for i in args.probelist.split(",")]
+   
+   plottrace=args.plottrace
+   if plottrace=="None":
+      plottrace=None
+   ndots=args.ndots
+   if ndots=="None":
+      ndots=None   
+   
+   mode=args.mode
+   
+   parser.set_defaults(keepoutput=False)
+   parser.set_defaults(plottrace=False)
+   
+   
+   keepoutput=args.keepoutput
+   
+   
+   endframe=args.endframe
+   main(pdbfiles,xtcfiles,plottrace,probes,ndots,mode,keepoutput,endframe)
---
+++ GlycoSHIELD-0.1.py
@@ -0,0 +1,221 @@
+__author__ = "Matt Sikora"
+__copyright__ = "Copyright 2021, GlycoSHIELD project"
+__credits__ = ["Cyril Hanus"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Matt Sikora"
+__email__ = "masikora@biophys.mpg.de"
+__status__ = "Development"
+
+import sys
+import numpy as np
+import MDAnalysis as mda
+from MDAnalysis.lib.distances import distance_array
+from MDAnalysis.coordinates.memory import MemoryReader
+from MDAnalysis.analysis import align
+from argparse import ArgumentParser
+
+         
+def main2(protpdb,protxtc,inputfile,zmax,zmin,threshold,mode,dryrun,shuffle_sugar,ignorewarn):
+   # parse input on every sugar to be added
+   inputlines = inputparser(inputfile)
+   
+   #~ threshold=1.0 # threshold for the distance search 
+   
+   # when limiting no of atoms - expand max sugar span by this much. Only atoms withing such radius from the sequon are considered for clash checking.
+   sugar_expand_factor = 1.5
+   # Load protein
+   # single frame:
+   if protxtc is None:
+      uprot=mda.Universe(protpdb,trajectory=True)
+   else:
+      uprot=mda.Universe(protpdb,protxtc)
+
+
+   if mode=="All":
+      selprot=uprot.select_atoms("all")
+      sugar_sel_suffix = "all"
+   elif mode=="CG":
+      selprot=uprot.select_atoms("name CA")
+      sugar_sel_suffix = 'name O5 or ( resname ANE5 and name O6 )'
+   protframe=0
+   # Iterate over protein states
+   for prottp in uprot.trajectory:
+      
+      # counter for "entropy"
+      occupancies = []
+      
+      # Iterate over sugars:
+      for protchain,resids_on_protein,resids_on_sugar,sugarpdb,sugarxtc,pdbout,xtcout in inputlines:
+         # load sugar
+         usugar=mda.Universe(sugarpdb,sugarxtc,in_memory=True)
+         # select (part of) the protein that is connected to the sugar
+         tripep = usugar.select_atoms('protein and resid {} and name N CA CO'.format(" ".join([str(i) for i in resids_on_sugar])))
+         # select sequon on protein
+         sequon = uprot.select_atoms('segid {} and resid {} and name N CA CO'.format(protchain, " ".join([str(i) for i in resids_on_protein])))
+
+         # the selection has to have same length for protein and peptide with the sugar!    
+         # Now select part of the peptide that should be transplanted onto protein. By default we do not transplant the part which gets selected.
+         # This way if we say that sugarresid is 1,2,3 and we are on a tripeptide - then nothing will get transplanted. 
+         # And if we have a long loop 1,...,40 and we select only 1,40, then 2,...,39 will still be transplanted.
+         
+         peptide_to_transplant = [i for i in usugar.select_atoms('protein').residues.resids if i not in resids_on_sugar]      
+         #~ print resids_on_sugar,uprot.atoms.resids
+         
+         if len(peptide_to_transplant)==0:
+            sugar_without_protein = usugar.select_atoms('not protein')
+         else:
+            sugar_without_protein = usugar.select_atoms('not protein or ( protein and resid {} )'.format(" ".join([str(i) for i in peptide_to_transplant])))
+         
+         # We should probably be more lenient with the distance check between the first atoms of the peptide and the protein, i.e. dont check residues 2 and 39 in the example above
+         if len(resids_on_sugar) != len(usugar.select_atoms('protein').residues.resids):
+            buffer=1 # skip 1 AA after the first fitting AA and one before the last
+            flanked_resids_on_sugar = sorted(list(set(usugar.select_atoms('protein').residues.resids)-set(resids_on_sugar)))[buffer:-buffer]
+            sugar_without_protein_flanked = usugar.select_atoms('not protein or ( protein and resid {} )'.format(" ".join([str(i) for i in flanked_resids_on_sugar])))
+         else:
+            sugar_without_protein_flanked = sugar_without_protein
+         
+         
+         if mode == "CG":
+            sugar_without_protein_flanked = sugar_without_protein_flanked.select_atoms(sugar_sel_suffix)
+            
+         # Narrow the selection (especially important for large systems)
+         
+         sugar_root = sequon.center_of_mass()
+         # Get size of the sugar:
+         sizex=np.max(sugar_without_protein_flanked.positions[:,0])-np.min(sugar_without_protein_flanked.positions[:,0])
+         sizey=np.max(sugar_without_protein_flanked.positions[:,1])-np.min(sugar_without_protein_flanked.positions[:,1])
+         sizez=np.max(sugar_without_protein_flanked.positions[:,2])-np.min(sugar_without_protein_flanked.positions[:,2])
+         maxdiam = np.max([sizex,sizey,sizez])*sugar_expand_factor
+         
+         # Generate list of coordinates.
+         reduced_prot_positions = selprot.atoms.positions[(selprot.atoms.positions[:,0]<(sugar_root[0]+maxdiam)) & (selprot.atoms.positions[:,0]>(sugar_root[0]-maxdiam)) & 
+                                                        (selprot.atoms.positions[:,1]<(sugar_root[1]+maxdiam)) & (selprot.atoms.positions[:,1]>(sugar_root[1]-maxdiam)) &
+                                                        (selprot.atoms.positions[:,2]<(sugar_root[2]+maxdiam)) & (selprot.atoms.positions[:,2]>(sugar_root[2]-maxdiam))]
+            
+         # Placeholder for a reconstructed glycoprotein
+         glycoprotein = mda.Merge(uprot.atoms,sugar_without_protein)
+              
+         coordinates = []
+         # iterate over the traj to remove clashing frames. Can be sequential or random
+         sugarframes = range(usugar.trajectory.n_frames)
+         
+         # randomize sugar conformers
+         if shuffle_sugar:
+            np.random.shuffle(sugarframes)
+            
+         for n_tp in sugarframes:
+            _=usugar.trajectory[n_tp]
+            #~ print tp.time
+            # Align on the protein
+            align.alignto(tripep,sequon)
+
+            # check for overlap with a membrane: remove all conformers that have atoms below Zmin
+            min=np.min(sugar_without_protein_flanked.positions[:,2])
+            max=np.max(sugar_without_protein_flanked.positions[:,2])
+            
+            if ( not min<zmin ) and ( not max>zmax ) :
+               
+               # check for the overlap
+               if threshold !=0:
+                  distarr=distance_array(reduced_prot_positions,sugar_without_protein_flanked.positions)
+                  if np.min(distarr)<threshold:
+                     # overlap - do nothing
+                     pass
+                  else:
+                     # OK - merge two systems copying protein and adding sugar.
+                     coordinates.append(np.vstack((uprot.atoms.positions,sugar_without_protein.positions)))
+               else:
+                  # Threshold 0 - go ahead
+                  coordinates.append(np.vstack((uprot.atoms.positions,sugar_without_protein.positions)))
+         coordinates=np.array(coordinates)
+         
+         # save counters
+         #~ occupancies.append(len(coordinates)/float(usugar.trajectory.n_frames))
+         occupancies.append(len(coordinates))
+         
+         
+         if len(coordinates)==0 and protxtc is None:
+            # no frames selected. If this is a protein trajectory just ignore this.
+            if not ignorewarn:
+               
+               raise BaseException("No frames were accepted for residues {} in chain {}!\nChange threshold or check if the sequon location is accessible!".format(resids_on_protein,protchain))
+            else:
+               print "WARNING!!!\nNo frames were accepted for residues {} in chain {}!\nThis means the sequon location is not accessible given the threshold\nYou decided to ignore this warning so we carry on!".format(resids_on_protein,protchain)
+               continue
+         # If iterating over protein traj - make sure to modify coordinates.
+         if protxtc is not None:
+            xtcout = xtcout.replace('.xtc','_protein_frame_{}.xtc'.format(protframe))
+         
+         # skip writing xtc,pdb
+         if not dryrun:
+            glycoprotein.load_new(coordinates, format=MemoryReader)
+            # Write new reference
+            glycoprotein.atoms.write(pdbout)
+            
+            # Write new trajectory
+            with mda.coordinates.XTC.XTCWriter(xtcout,n_atoms=glycoprotein.atoms.n_atoms) as w:
+               for tp in glycoprotein.trajectory:
+                  w.write(glycoprotein.atoms)
+                  
+         del usugar,glycoprotein
+         
+      # Main output printout
+      print protframe," ".join([str(i) for i in occupancies])
+      
+      # Increment frame counter
+      protframe+=1
+      
+def inputparser(fname):
+   inputs=[]
+   with open(fname) as f:
+      for line in f:
+         if "#" not in line:
+            l=line.split()
+            protchain=l[0]
+            protsequonresid=[int(i) for i in l[1].split(",")]
+            sugarsequonresid=[int(i) for i in l[2].split(",")]
+            sugarpdb=l[3]
+            sugarxtc=l[4]
+            pdbout=l[5]
+            xtcout=l[6]
+            inputs.append([protchain,protsequonresid,sugarsequonresid,sugarpdb,sugarxtc,pdbout,xtcout])
+   return inputs
+   
+   
+if __name__=="__main__":
+   parser=ArgumentParser()
+   parser.add_argument('--protpdb', dest='protpdb', help='pdb file for protein')
+   parser.add_argument('--protxtc', dest='protxtc', help='xtc file for protein, optional if one needs multiple states of the protein')
+   parser.add_argument('--inputfile', dest='inputfile', help='Input file with each line representing a sequon')
+   parser.add_argument('--threshold',dest='threshold', help='threshold in A for checking for the overlap between sugar and protein.')
+   parser.add_argument('--mode',dest='mode', help='do we take all atoms "All" or only Calpha "CG" when calculating distances. CG useful for large proteins but remember to change threshold! A good value here would be 0.7 for All atom and 3.5 for CG')
+   parser.add_argument('--zmin',dest='zmin', help='position of the membrane below which we do not accept the glycan',default=-110000)
+   parser.add_argument('--zmax',dest='zmax', help='position of the membrane above which we do not accept the glycan',default=110000)
+   parser.add_argument('--dryrun', dest='dryrun', action='store_true',help="Dry run, do not write pdb/xtc files")
+   parser.add_argument('--no-dryrun', dest='dryrun', action='store_false')
+   parser.add_argument('--shuffle-sugar', dest='shuffle_sugar', action='store_true',help='randomize sugar frames when grafting onto protein')
+   parser.add_argument('--no-shuffle-sugar', dest='shuffle_sugar', action='store_false')
+   parser.add_argument('--ignorewarn', dest='ignorewarn', action='store_true',help="Ignore cases when no sugar is implanted")
+   parser.add_argument('--no-ignorewarn', dest='ignorewarn', action='store_false')   
+   parser.set_defaults(dryrun=False)
+   parser.set_defaults(ignorewarn=False)
+   parser.set_defaults(protxtc=None)
+   parser.set_defaults(shuffle_sugar=False)
+   
+   parser.set_defaults(mode="All")
+
+   args = parser.parse_args()
+   
+   protpdb = args.protpdb
+   protxtc = args.protxtc
+   inputfile=args.inputfile
+   threshold=float(args.threshold)
+   zmax=float(args.zmax)
+   zmin=float(args.zmin)
+   mode=args.mode
+   dryrun=args.dryrun
+   shuffle_sugar=args.shuffle_sugar
+   ignorewarn=args.ignorewarn
+   
+   main2(protpdb,protxtc,inputfile,zmax,zmin,threshold,mode,dryrun,shuffle_sugar,ignorewarn)
