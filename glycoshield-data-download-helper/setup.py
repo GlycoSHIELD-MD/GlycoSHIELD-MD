fa841d0e8839e835454c608afd87d852b366c35e
Klaus Reuter <khr@mpcdf.mpg.de>
2021-12-28 18:54:48 +0100

    add proposal for zenodo download helper

---
+++ glycoshield-data-download-helper/setup.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+from setuptools import setup
+from setuptools.command.develop import develop as _develop
+from setuptools.command.install import install as _install
+
+def zenodo_downloader():
+    doi="10.5281/zenodo.5337276"
+    out=os.path.join(os.path.expanduser("~"), "GLYCAN_LIBRARY")
+    if not os.path.exists(out):
+        os.mkdirs(out)
+    cmd="zenodo_get --output-dir={} {} &>{}/zenodo_get.log".format(out, doi, out)
+    subprocess.check_call(cmd, shell=True)
+
+class develop(_develop):
+    def run(self):
+        zenodo_downloader()
+        _develop.run(self)
+
+class install(_install):
+    def run(self):
+        zenodo_downloader()
+        _install.run(self)
+
+setup(
+    name='glycoshield-data-download-helper',
+    version='0.1',
+    packages=[],
+    cmdclass={
+        'develop': develop,
+        'install': install,
+    },
+)
+
