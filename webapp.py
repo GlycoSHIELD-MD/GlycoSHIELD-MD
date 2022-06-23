e1c580062889ff42a94c53b981b390b27f916bbc
Klaus Reuter <khr@mpcdf.mpg.de>
2022-06-23 16:54:06 +0200

    update environment.yml, clean webapp.py

--- webapp.py
+++ webapp.py
@@ -1,13 +1,5 @@
-import os
-import base64
-import getpass
 import streamlit as st
 import glycoshield.app as app
-
-import streamlit_modal as modal
-import streamlit.components.v1 as components
-from st_click_detector import click_detector
-
 
 st.set_page_config(
     page_title="GlycoSHIELD",
