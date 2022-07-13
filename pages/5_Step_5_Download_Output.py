7b730f3701da90e3517eb81dc23bd4f6747359f2
Matt Sikora <matsikora@gmail.com>
2022-07-13 12:42:54 +0200

    visualisation message on last page

--- pages/5_Step_5_Download_Output.py
+++ pages/5_Step_5_Download_Output.py
@@ -6,7 +6,20 @@ st.set_page_config(
     layout="wide"
 )
 app.show_header(title="Download Output", show_glycoshield_logo=False)
+st.markdown("""
+You can download the multimodel file with selected number of glycan conformers or the zipped folder 
+containing a full GlycoSHIELD run output. 
 
+The glycan conformers can be visualised using the standard visualisation tools, e.g. 
+*  [Pymol](https://pymol.org/2/)
+Open the output pdb file na type ```set all_states, on``` in the console to see all glycan conformers.
+*  [VMD](https://www.ks.uiuc.edu/Research/vmd/)
+After opening the output file navigate to ```Graphics``` > ```Representations``` and look for a ```trajectory``` tab. 
+There, in the "Draw Multiple Frames" field put ```1:1:X``` where X is the total 
+number of conformers you would like to see.
+	
+
+""")
 # Treat pdb trajectory separately
 app.zip_pdb_trajectory()
 data_pdb, size_pdb = app.get_webapp_output_pdbtraj()
