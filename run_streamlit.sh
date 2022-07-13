47b7b661757c250f846ce62faa4db64642abda31
Klaus Reuter <khr@mpcdf.mpg.de>
2022-07-13 17:25:38 +0200

    fix run_streamlit.sh

--- run_streamlit.sh
+++ run_streamlit.sh
@@ -3,8 +3,14 @@
 # Launch the streamlit-based webapp, either locally on your computer or automatically
 # on binder via https://gitlab.mpcdf.mpg.de/khr/jupyter-streamlit-proxy
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}
+
 streamlit run Welcome.py \
     --server.headless true \
     --browser.gatherUsageStats false \
-    --browser.serverAddress "127.0.0.1" $*
+    --browser.serverAddress "0.0.0.0" \
+    "$@"
+
+#    --browser.serverAddress "127.0.0.1" "$@"
 
