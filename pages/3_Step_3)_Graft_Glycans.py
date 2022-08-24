2ef26815aa159eeded09cab76b5096ee627d3a70
masikora <masikora@biophys.mpg.de>
2022-08-24 13:46:23 +0200

    Added a button setting 'skip' value

--- pages/3_Step_3)_Graft_Glycans.py
+++ pages/3_Step_3)_Graft_Glycans.py
@@ -26,7 +26,7 @@ glycostraj_progressbar_2 = st.progress(0)
 # Set the mode and threshold values, no of frames to download (could be put in a single line)
 st.markdown("""Set parameters for grafting:""")
 # Multi-column input
-but1, but2, but3 = st.columns(3)
+but1, but2, but3, but4 = st.columns(4)
 with but1:
     #~ st.header("Set grafting mode")
     glycoshield_mode_str = st.selectbox(label="Set grafting mode", options=("CG", "All"), index=0, help="All (all atoms): graft a conformer if a distance between any of the protein and glycan atoms is larger than the cut-off; CG (coarse-grained): graft a conformer if a distance between protein alpha carbons and glycan ring oxygens is larger than the cut-off. Recommended for large protein structures!")
@@ -37,13 +37,16 @@ with but3:
     #~ st.header("Set grafting mode")
     glycotraj_numpdbframes_str = st.text_input(label="Number of conformers for PDB download", value="30", help="Warning: large number of conformers can generate huge files!")
 
+with but4:
+    glycotraj_skip_str = st.text_input(label="Use only every n-th glycan conformer", value="10", help="Note: Smaller value will generate more complete shields, at a risk of exceeding the application memory for large proteins")
+
 glycoshield_threshold = float(glycoshield_threshold_str)
 glycotraj_numpdbframes = int(glycotraj_numpdbframes_str)
-
+glycotraj_skip = int(glycotraj_skip_str)
 if st.button("Run glycoSHIELD ..."):
-    skip=10
+    
     app.display_image(app.glycoshield_logo_anim, progress_image_obj, image_style=app.glyco_logo_image_style)
-    app.run_glycoshield(glycoshield_progressbar, mode=glycoshield_mode_str, threshold=glycoshield_threshold, skip=skip)
+    app.run_glycoshield(glycoshield_progressbar, mode=glycoshield_mode_str, threshold=glycoshield_threshold, skip=glycotraj_skip)
     app.run_glycotraj(glycostraj_progressbar_1, glycostraj_progressbar_2, pdbtrajframes=glycotraj_numpdbframes)
 
 app.check_glycoshield(glycoshield_progressbar)
