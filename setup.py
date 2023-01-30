881d5410347b6dbd204b3de4e7f72ffc87e5b540
Klaus Reuter <klaus.reuter@mpcdf.mpg.de>
2023-01-30 17:24:44 +0100

    prepare config for binder-production to distribute pods better across workers

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
-import os
-import setuptools
-
-class CleanCommand(setuptools.Command):
-    """Custom clean command to remove unnecessary files."""
-    user_options=[]
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        cwd=os.path.dirname(os.path.abspath(__file__))
-        patterns=['*.pyc', '__pycache__', '*.egg-info']
-        for p in patterns:
-            os.system("find {} -name \'{}\' -exec rm -rf {{}} \\; -print".format(cwd, p))
-
-setuptools.setup(
-    cmdclass={'clean': CleanCommand},
+#from setuptools import setup
+from distutils.core import setup
+setup(
+    name='GlycoSHIELD',
+    version='0.1',
+    py_modules=['glycoshield',],
+    #packages=['glycoshield',],
 )
